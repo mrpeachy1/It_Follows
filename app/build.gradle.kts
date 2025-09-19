6863a9a8694254eabbd0bf31f0edfe98d29e9076
tomaz <tomazstrojnik@gmail.com>
2025-09-19 12:48:51 -0400

    30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,15 +14,19 @@ android {
         applicationId = "com.itfollows.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+
+            // Enables resource shrinking.
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
