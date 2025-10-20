e42f96e4865528abb8a6e5ff535e8a1a2300b29f
mrpeachy1 <tomazstrojnik@gmail.com>
2025-10-20 11:43:59 -0400

    30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.itfollows.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
